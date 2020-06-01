80939a17bb95e8d9a694cce2c751019f5d190225
Junji UEHARA <uehera.junji@ntt-tx.co.jp>
2020-06-01 09:34:00 +0900

    add if special form

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import program from 'commander';
 import fs, { appendFileSync } from 'fs';
 import yaml from 'js-yaml';
-import { exec } from 'child_process';
 
-function parseYamlString(yamlText: string) {
+type AST = object;
+
+function parseYamlString(yamlText: string): AST {
   return yaml.safeLoad(yamlText);
 }
 
-function parseYamlFile(filename: string) {
+function parseYamlFile(filename: string): AST {
   const yamlText = fs.readFileSync(filename, 'utf8');
   return parseYamlString(yamlText);
 }
@@ -39,24 +40,32 @@ const predefinedFunctions = {
   '<<': (a: any, b: any) => a << b,
   '>>': (a: any, b: any) => a >> b,
   '>>>': (a: any, b: any) => a >>> b,
+  $if: (condPart: AST, thenPart: AST, elsePart: AST) => {
+    const cond = evalYaml(condPart);
+    if (!!cond) {
+      return evalYaml(thenPart);
+    } else {
+      return evalYaml(elsePart);
+    }
+  },
 };
 
-const applyFunc = (funcName: string, args: any[]) => {
+const applyNativeFunc = (funcName: string, args: AST[]) => {
   const programText = `"use strict";return ${funcName}(...args);`;
   return new Function('args', programText)(args);
 };
 
-function applyFunction(funcName: string, arg: any[]) {
+function applyPredefinedFunction(funcName: string, arg: AST[]) {
   if (Object.keys(predefinedFunctions).indexOf(funcName) !== -1) {
     const result = (predefinedFunctions as any)[funcName](...arg);
     return result;
   } else {
-    const result = applyFunc(funcName, arg);
+    const result = applyNativeFunc(funcName, arg);
     return result;
   }
   return undefined;
 }
-function evalYaml(script: object): any {
+function evalYaml(script: object): AST {
   let result = null;
   if (Array.isArray(script)) {
     for (const elem of script) {
@@ -68,12 +77,16 @@ function evalYaml(script: object): any {
     for (const key of Object.keys(script)) {
       const arg = (script as any)[key];
       if (Array.isArray(arg)) {
-        result = applyFunction(
-          key,
-          arg.map((elem) => evalYaml(elem))
-        );
+        if (key.startsWith('$')) {
+          result = applyPredefinedFunction(key, arg);
+        } else {
+          result = applyPredefinedFunction(
+            key,
+            arg.map((elem) => evalYaml(elem))
+          );
+        }
       } else {
-        result = applyFunction(key, [evalYaml(arg)]);
+        result = applyPredefinedFunction(key, [evalYaml(arg)]);
       }
     }
   } else {
